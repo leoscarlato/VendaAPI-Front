d590de75bdd5b67dd2ffef70d7cbe76963cefd20
C0D8 <matheusaguiar362@gmail.com>
2023-10-30 15:05:20 -0300

    a

--- venda-api/src/App.jsx
+++ venda-api/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ venda-api/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+// create a login page for the user to login and regiter the route in App.jsx
+
+// Path: venda-api/src/pages/Login.jsx
+
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+function Login(){
+
+
+    const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+}
